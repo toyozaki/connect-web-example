dff225aa69527c447f419d2b24041556f62710c2
toyozaki <61676668+toyozaki@users.noreply.github.com>
2022-09-10 20:33:45 +0900

    Study protocols

--- src/hooks/useClient.ts
+++ src/hooks/useClient.ts
@@ -7,6 +7,7 @@ import { ServiceType } from "@bufbuild/protobuf";
 import { useMemo } from "react";
 import { logger } from "../utils/interceptor";
 
+// gRPC-webを使う時は, createGrpcWebTransportを用いる
 const transport = createConnectTransport({
   baseUrl: "https://demo.connect.build",
   interceptors: [logger]
